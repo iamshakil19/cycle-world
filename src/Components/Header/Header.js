14003e63673377482d8994ea8af7bf116d7051c6
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-04-29 01:00:17 +0600

    not found page complete

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <p>Admin</p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Header;
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './NotFound.css'
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not Found</h2>
+        <div className='text-center mt-40'>
+            <h2 className='text-5xl font-bold sm:text-9xl mb-4 text-zinc-700'>404</h2>
+            <p className='text-xl sm:text-4xl font-medium mb-4 text-zinc-700'>Not Found</p>
+            <p className='px-5'>The resource requested could not be found on this server!</p>
         </div>
     );
 };
